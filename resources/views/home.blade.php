2ee6773f9ab0c54949d7781f7f35211cf948f227
RahmatAdmin <taufikrahmat14720@gmail.com>
2024-01-17 17:17:06 +0700

    web me

---
+++ resources/views/home.blade.php
@@ -0,0 +1,173 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Bootstrap Example</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body class="p-3 m-0 border-0 bd-example m-0 border-0">
+
+    <!-- Example Code -->
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">WEB.ME</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown link
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- End Example Code -->
+
+    <section>
+
+        <div class="content">
+            <div>
+                <div class="box">
+                    <img src="/img/me.png" alt="" srcset="">
+                    <svg viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M220,384.84444824668265C253.27959893710204,385.4935900888671,286.63841004647793,418.5655347455084,316.5371936252621,403.93625482287837C346.18015258581465,389.4321482184654,341.56000279148384,343.9979386598398,359.7527332708479,316.4643467758448C377.47839505895837,289.63763496910667,413.98439676525527,275.6825740765812,421.87611575824553,244.51219834057287C429.80950695671834,213.17722750129343,414.29953665877395,180.85328243535037,401.61388866407094,151.12293719078613C389.25371105807847,122.15537103541679,373.7756916260855,93.85845526116955,349.1931997990426,74.17102335676046C325.08575761167276,54.86404545587881,293.8832930218372,49.249757325727934,264.1179839454516,41.00630294848113C233.6309671997665,32.56297279536676,201.12870048457455,12.84595138808811,171.9580202167889,25.086026013710153C142.1005426616902,37.61428212746396,139.33995324274406,80.20280071909872,116.35432907874859,103.0082222016449C95.07368058142933,124.12203468464335,54.23964413859244,125.07927957819723,43.116877413247465,152.91708010925996C32.03449380979411,180.65381070693292,57.83899295030339,209.6060526654611,61.941332959088264,239.19179681757436C65.96117348080452,268.18256134540826,56.32229503208109,298.3405481323123,67.05669928843068,325.56913808826863C78.81077993550898,355.38421069085143,95.53678260833122,388.6269608391738,125.41935245588371,400.2083677252759C155.6400951422492,411.92083821347444,187.59512800937873,384.2123685650885,220,384.84444824668265" fill="#E8DAFF" />
+                    </svg>
+                </div>
+
+                <h1>Rahmat Taupik Rinaldi</h1>
+                <h2>Web Development</h2>
+                <p>
+                    I am a creative person and like to find alternative solutions to various problems. My enthusiasm for the world of technology makes me continue to learn and achieve several achievements. I have handled and taken part in several projects in the world of technology
+                </p>
+            </div>
+
+
+            <div class="footer">
+                <button class="btn-2">Contact</button>
+                <button class="btn-1">Download CV</button>
+            </div>
+
+            <h1>My Skill</h1>
+            <p>
+                in the world of web development I focus on Front-end and this is the technology that I use
+            </p>
+            <div class="box-skill">
+                <img src="/img/icon (1).png" alt="" class="icon">
+                <img src="/img/icon (2).png" alt="" class="icon">
+                <img src="/img/icon (3).png" alt="" class="icon">
+                <img src="/img/icon (4).png" alt="" class="icon">
+                <img src="/img/icon (5).png" alt="" class="icon">
+                <img src="/img/icon (6).png" alt="" class="icon">
+            </div>
+
+
+            <h1 class="ex">Experience</h1>
+            <p>
+                I have some experience that is directly oriented to the world of work and handling real projects
+            </p>
+
+            <hr>
+            <ul>
+                <li>
+                    <h2 class="adm">Administrator</h2>
+                </li>
+
+                <h5>09/2023 - 11 - 2023 </h5>
+            </ul>
+            <div class="glass-card">
+                <div class="glass-card-content">
+                    <h2>PT. Riau Wisata Hati</h2>
+                    <ul>
+                        <li>
+                            Riau Wisata Hati (RWH) is the best Umrah travel agency that provides various kinds of Umrah packages ranging from Regular Umrah to Promo Umrah.
+
+                            You can choose the cost of Umrah with Riau Wisata Hati (RWH) according to your needs and interests. RWH provides the cheapest Umrah package prices in its class
+                        </li>
+                    </ul>
+
+                    <Label> Download Resume
+                        <a href="resume"></a>
+                    </Label>
+                </div>
+            </div>
+            <br>
+            <ul>
+                <li>
+                    <h2 class="adm">Web Development</h2>
+                </li>
+
+                <h5>09/2021 - Now</h5>
+            </ul>
+            <div class="glass-card">
+                <div class="glass-card-content">
+                    <h2>Front-End Web</h2>
+                    <ul>
+                        <li>
+                            a professional in the field of information technology who is responsible for developing and maintaining the front end of a website.
+                        </li>
+
+                        <li>
+                            Responsbility
+                        </li>
+                        <ol>
+                            <li>
+                                designing user interface
+                            </li>
+                            <li>
+                                responsive design
+                            </li>
+                            <li>
+                                optimization of browsing speed
+                            </li>
+                        </ol>
+                    </ul>
+
+                    <Label> Download Resume
+                        <a href="resume"></a>
+                    </Label>
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </section>
+
+    <div class="foot">
+        <div class="medsos">
+        <input type="text" placeholder="Comment"> <button id="submit" type="submit">Send</button>
+        </div>
+    </div>
+
+    <script src="/js/main.js"></script>
+
+</body>
+
+</html>
